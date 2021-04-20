3344b1483ece43f1060b30c5e379e4fe9b44885e
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-04-20 08:29:11 -0400

    new key code

--- Key.java
+++ Key.java
@@ -3,13 +3,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     private boolean isDown;
+    private String key;
+    private String sound;
+    
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile )
     {
+       key=keyName;
+       sound=soundFile;
     }
-
+     // methods omitted.
     /**
      * Do the action for this key.
      */
@@ -17,15 +22,23 @@ public class Key extends Actor
     public void act()
     {
         
-         if ( !isDown&& Greenfoot.isKeyDown("g"))
+         if ( !isDown&& Greenfoot.isKeyDown(key))
         {
             setImage("white-key-down.png");
+            play ();
             isDown= true;
         }
-        if ( isDown && !Greenfoot.isKeyDown("g"))
+        if ( isDown && !Greenfoot.isKeyDown(key))
         {
             setImage("white-key.png");
             isDown=false;
          }
         }
+     /*
+      * play the note of this key  
+      */
+     public void play()
+     {
+         Greenfoot.playSound (sound);
+     }
 }
