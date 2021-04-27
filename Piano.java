e470d457551dc0ff91e48d2437ad2845b339d003
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-04-27 08:37:57 -0400

    added key codes

--- Piano.java
+++ Piano.java
@@ -8,11 +8,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+   private String[] whiteKeys= 
+   {"a","s","d","f","g","h","j","K","l",";","'","\\"};
+   private String[] whiteNotes=
+   {"3c","3d","3e","3f","3g","3a","3b","4c","4d","4e","4f","4g"};
+   // constructor and methods omitted 
     /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
+        
+        for (int i=0; i < whiteKeys.length; i++)
+        {
+            addObject(new Key(whiteKeys[i], "3a.wav"), 54+i*63,140);
+        }                                                                                                                                                                 
     }
 }
